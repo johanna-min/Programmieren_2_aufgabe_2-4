d6e749230d8e169c42d5c3a32171f8a068dcf505
johanna-min <j.helfer@mci4me.at>
2026-06-03 15:51:08 +0200

    Änderung x - Achse im Line Plot

--- advanced_power_curve.py
+++ advanced_power_curve.py
@@ -16,7 +16,7 @@ def find_best_power(df, windowsize_seconds, umrechnungsfaktor = 1): #das ... da 
     # übrig bleibt die anzahl (genau das brauchen wir hier)
     windowsize_rows = windowsize_seconds * umrechnungsfaktor
 
-    rolling_mean = df.rolling(windowsize_rows).mean()
+    rolling_mean = df["PowerOriginal"].rolling(windowsize_rows).mean()
     max_power = rolling_mean.max()
 
     return max_power
@@ -26,21 +26,24 @@ def find_all_windows(df, window_list, umrechnungsfaktor = 1):
     for windowsize_seconds in window_list:
         best_power = find_best_power(df, windowsize_seconds, umrechnungsfaktor)
         power_list.append(best_power)
-        # df_power = pd.DataFrame({
-        # "time in seconds": window_list,
-        # "best_power": power_list
-        # })
-
+    df_power = pd.DataFrame({
+        "time in seconds": window_list,
+        "best_power": power_list
+        })
 
     return df_power
 
 def make_power_curve(df_power):
     fig = px.line(df_power, x= "time in seconds", y= "best_power")
+
+    fig.update_xaxes(type="log")
+    
     return fig
 
 if __name__ == "__main__":
     df = read_data()
-    window_list = [10, 20, 30, 60, 120, 30, 1200, 3600, 7200]
+   # window_list = [10, 20, 30, 60, 120, 30, 1200, 3600, 7200]
+    window_list = np.arange(1, 7201)
     df_power = find_all_windows(df, window_list)
     fig = make_power_curve(df_power)
     fig.show()
